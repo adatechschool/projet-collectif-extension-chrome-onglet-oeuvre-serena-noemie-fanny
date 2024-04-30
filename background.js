c4dc359f04dea39e519aec98a281e82f22e03d8a
neonlazerneon <fannyjeannesimon@gmail.com>
2024-04-30 16:26:09 +0200

    demarrage fonction js

---
+++ background.js
@@ -0,0 +1,3 @@
+let images = ["EllenvonUnwerth.jpg","laurenbacall.jpg","lebovitz.jpg","louisedahl","sarahmoon.jpg"];
+
+const randomImage = "PROJETimages" + images[Math.floor(Math.random() * images.length)];
