3a3a084c37aaa30c606cc94f1099410c4a079ff7
Noemie-Gyc <goyec.noemie@gmail.com>
2024-05-02 12:04:41 +0200

    Affichage image via js

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
-let images = ["EllenvonUnwerth.jpg","laurenbacall.jpg","lebovitz.jpg","louisedahl","sarahmoon.jpg"];
+let images = ["./images/EllenvonUnwerth.jpg","./images/laurenbacall.jpg","./images/lebovitz.jpg"];
 
-const randomImage = "PROJETimages" + images[Math.floor(Math.random() * images.length)];
+// const randomImage = document.createElement("images") + images[Math.floor(Math.random() * images.length)];
+
+const image = document.createElement("img");
+image.src = "./images/EllenvonUnwerth.jpg";
+document.body.appendChild(image);
+
+
+// function affichageImage() {
+
+//     let maDiv = document.getElementById('mainImage');
+//     let monImage = document.createElement('img');
+//     monImage.src = "./images/EllenvonUnwerth.jpg";
+//     maDiv.appendChild(monImage)
+
+// }
+
+// affichageImage();
+
