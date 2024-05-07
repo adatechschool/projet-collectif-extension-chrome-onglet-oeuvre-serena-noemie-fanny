1a8333244bd284ae7485a1323099c3428df16e8c
Noemie-Gyc <goyec.noemie@gmail.com>
2024-05-07 16:37:58 +0200

    Ajout horloge

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-photoRandom= Math.floor((Math.random()*21)+1)
+photoRandom= Math.floor((Math.random()*30)+1)
   
 let legendDictionnary = {
     1 : ["Imogen Cunningham", " Two Callas", " vers 1925 (Dulwich Picture Gallery, Londres, Royaume-Uni)"],
@@ -23,7 +23,17 @@ let legendDictionnary = {
     19 : ["Berenice Abbott", " Canyon : Broadway and Exchange Place, Manhattan", " 1936 (Moma, New-York)"],
     20 : ["Ninagawa Mika", " « Untitled »from the series Utsurundesu", " 2018-19 (Ninagawa Mika)"],
     21 : ["Hiromix", " Seventeen Girl Days", " 1995 (Abe Books)"],
+    22 : ["Cindy Sherman", " Untitled", " 2023 (Courtesy the artist and Hauser & Wirth Publishers)"],
+    23 : ["Cindy Sherman", " Untitled", " 1981 (Cindy Sherman/Courtesy of Christie's)"],
+    24 : ["Graciela Iturbide", " Procesión", " 1984 (Cortesía Palacio de Cultura Citibanamex)"],
+    25 : ["Graciela Iturbide", " Nuestra señora de las iguanas", " 1979 (Cortesía Palacio de Cultura Citibanamex-Palacio de Iturbide)"],
+    26 : ["Graciela Iturbide", " Magnolia", " 1986 (Fundación MAPFRE)"],
+    27 : ["Delphine Diaw Diallo", " Seventeen Girl Days", " 1995 (Abe Books)"],
+    28 : ["Chen Man", " Funky Greatwall", " 1995 (Abe Books)"],
+    29 : ["Lebohang Kganye", " Ka 2 phisi yaka e pinky II, from the series: Ke Lefa Laka: Her-Story", " 2013 (Courtesy: AFRONOVA GALLERY, Johannesburg)"],
+    30 : ["Zanele Muholi", " Aphelile IV, Durban", " 2020 (Courtesy of the artist. Yancey Richardson Gallery, New York and Stevenson Gallery, Cape Town/Johannesburg)"]
 }
+
 document.getElementById('icones').innerHTML += "<img src=\"./icons/frida2.svg\"/>"
 
 document.getElementById('mainImage').innerHTML += "<img src=\"./images/photo"+ photoRandom+".jpg\"/>"
@@ -32,4 +42,90 @@ document.getElementById('legend1').innerHTML += legendDictionnary[photoRandom][0
 
 document.getElementById('legend').innerHTML += legendDictionnary[photoRandom][1]
 
+function initHeureLocale(){
+    var date = new Date;
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hours = date.getHours();
+  
+    // Create an object with each hand and it's angle in degrees
+    var hands = [
+      {
+        hand: 'hours',
+        angle: (hours * 30) + (minutes / 2)
+      },
+      {
+        hand: 'minutes',
+        angle: (minutes * 6)
+      },
+      {
+        hand: 'seconds',
+        angle: (seconds * 6)
+      }
+    ];
+    // Loop through each of these hands to set their angle
+    for (var j = 0; j < hands.length; j++) {
+      var elements = document.querySelectorAll('.' + hands[j].hand);
+      for (var k = 0; k < elements.length; k++) {
+          elements[k].style.webkitTransform = 'rotateZ('+ hands[j].angle +'deg)';
+          elements[k].style.transform = 'rotateZ('+ hands[j].angle +'deg)';
+          // If this is a minute hand, note the seconds position (to calculate minute position later)
+          if (hands[j].hand === 'minutes') {
+            elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
+          }
+      }
+    }
+  }
+initHeureLocale();
 
+// permet de convertir l'heure dans l'angle des aiguilles correspondants. Elle fait une boucle sur chaque aiguille lui donne le bon angle par les proprietes style.transform et rotateZ
+
+function setUpMinuteHands() {
+    // Find out how far into the minute we are
+    var containers = document.querySelectorAll('.minutes-container');
+    var secondAngle = containers[0].getAttribute("data-second-angle");
+    if (secondAngle > 0) {
+      // Set a timeout until the end of the current minute, to move the hand
+      var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
+      setTimeout(function() {
+        moveMinuteHands(containers);
+      }, delay);
+    }
+  }
+  setUpMinuteHands();
+function moveMinuteHands(containers) {
+    for (var i = 0; i < containers.length; i++) {
+      containers[i].style.webkitTransform = 'rotateZ(6deg)';
+      containers[i].style.transform = 'rotateZ(6deg)';
+    }
+    // Then continue with a 60 second interval
+    setInterval(function() {
+      for (var i = 0; i < containers.length; i++) {
+        if (containers[i].angle === undefined) {
+          containers[i].angle = 12;
+        } else {
+          containers[i].angle += 6;
+        }
+        containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
+        containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
+      }
+    }, 60000);
+  }
+  moveMinuteHands();
+
+  function moveSecondHands() {
+    var containers = document.querySelectorAll('.seconds-container');
+    setInterval(function() {
+      for (var i = 0; i < containers.length; i++) {
+        if (containers[i].angle === undefined) {
+          containers[i].angle = 6;
+        } else {
+          containers[i].angle += 6;
+        }
+        containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
+        containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
+      }
+    }, 1000);
+  }
+  moveSecondHands()
+  
